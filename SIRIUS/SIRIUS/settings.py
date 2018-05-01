7c6336bedc3afdc0e5ed5f9cd0372ad34a989982
Guillermo Betancourt <guillermojbetancourt@gmail.com>
2018-05-01 10:27:47 -0400

    typos fixed

--- SIRIUS/SIRIUS/settings.py
+++ SIRIUS/SIRIUS/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	'interface.apps.InterfaceConfig',
+    "index",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Caracas'
 
 USE_I18N = True
 
