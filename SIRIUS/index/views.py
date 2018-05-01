84f2b5f23fa711335c58a893a04c4dc36cb8161b
Maria B <mabraca18@gmail.com>
2018-05-01 18:31:18 -0400

    Login/Logout/Register

--- SIRIUS/index/views.py
+++ SIRIUS/index/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django import forms
+from .forms import UserRegistrationForm
 # Create your views here.
 
 def index(request):
 	return render(request, "index/index.html", {})
 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            userObj = form.cleaned_data
+            username = userObj['username']
+            email =  userObj['email']
+            password =  userObj['password']
+            if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
+                User.objects.create_user(username, email, password)
+                user = authenticate(username = username, password = password)
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                raise forms.ValidationError('Parece que el username o correo ya existe. Prueba con otro.')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form' : form})
