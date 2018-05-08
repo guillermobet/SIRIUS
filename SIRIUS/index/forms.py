6ab196fbe2e6d78b8fb3fc650e9e8a3ec237d148
Guillermo Betancourt <guillermojbetancourt@gmail.com>
2018-05-08 12:18:03 -0400

    updated register html

--- SIRIUS/index/forms.py
+++ SIRIUS/index/forms.py
@@ -4,27 +4,34 @@ class UserRegistrationForm(forms.Form):
     username = forms.CharField(
         required = True,
         label = 'Username',
-        max_length = 32
+        max_length = 32,
+        widget = forms.TextInput({"placeholder": "Username"})
+
     )
     full_name = forms.CharField(
         required = True,
         label = 'Full name',
-        max_length = 60
+        max_length = 60,
+        widget = forms.TextInput({"placeholder": "Full Name"})
+
     )
     telephone= forms.CharField(
         required = False,
         label = 'Telephone',
-        max_length = 11
+        max_length = 11,
+        widget = forms.TextInput({"placeholder": "Telephone"})
+
     )
     email = forms.CharField(
         required = True,
         label = 'Email',
         max_length = 32,
+        widget = forms.TextInput({"placeholder": "Email"})
     )
     password = forms.CharField(
         required = True,
         label = 'Password',
         max_length = 32,
-        widget = forms.PasswordInput()
+        widget = forms.PasswordInput({"placeholder": "Password"})
     )
 
