dff7c9e23d1f3d390e9d41786e940199c02817da
Juan <12-11469@usb.ve>
2018-06-05 16:06:20 -0400

    Finished populateDB

--- SIRIUS/index/management/commands/populateDB.py
+++ SIRIUS/index/management/commands/populateDB.py
@@ -1,6 +1,18 @@
 from django.core.management.base import BaseCommand
 from index.models import MetaHeuristic, MetaCriteria
 
+def add_search_relevance(relevance_string):
+	aux = relevance_string.split()
+	output = []
+	
+	for i in range(0, len(aux)):
+		if(i == 4):
+			output.append('CR')
+		output.append(aux[i])
+		
+	output = " ".join(output)
+	return output
+	
 # THIS STILL NEED WORK
 class Command(BaseCommand):
 	args = 'foo bar..'
@@ -11,6 +23,52 @@ class Command(BaseCommand):
 		MetaHeuristic.objects.all().delete()
 	
 	def _populate_db(self):
+		try:
+			f = open('static/baseMetaScheme.txt', 'r')
+		except FileNotFound:
+			print('File baseMetaScheme.txt not in static folder')
+			exit()
+			
+		# read from file and create evrything
+		line = None
+		while(line != '--EOF--'):
+			
+			# Read MetaHeuristic
+			heuristic_name = f.readline()[:-1]
+			heuristic_acronym = f.readline()[:-1]
+			heuristic_relevance = f.readline()[:-1]
+			# Create MetaHeuristic
+			heuristic = MetaHeuristic.objects.create(
+				name = heuristic_name,
+				acronym = heuristic_acronym,
+				#relevance = heuristic_relevance,
+				comment = ''
+			)
+			line = f.readline()
+			line = f.readline()
+			while(line != '' and line != '--EOF--'):
+				# Read MetaCriterion
+				line = line[:-1]
+				line = line.split(': ')
+				criterion_name = line[1]
+				criterion_acronym = line[0]
+				criterion_relevance = f.readline()[:-1]
+				criterion_attribute = f.readline()[:-1]
+				
+				# Create MetaCriterion
+				MetaCriteria.objects.create(
+					heuristic = heuristic,
+					name = criterion_name,
+					acronym = criterion_acronym,
+					#relevance = add_search_relevance(criterion_relevance),
+					metric = '',
+					atribute = criterion_attribute,
+					comment = ''
+				)
+				
+				line = f.readline()[:-1]
+				
+		"""				
 		heuritic_aspectos_generales = MetaHeuristic.objects.create(
 			name = 'Aspectos Generales',
 			acronym = 'AG',
@@ -278,6 +336,7 @@ class Command(BaseCommand):
 			atribute = 'cualitativo',
 			comment = ''
 		)
+		"""
 		
 	def handle(self, *args, **options):
 		self._clean_db()
