e6220614ace4d9d17b245e493292f94775c55592
Juan <12-11469@usb.ve>
2018-06-15 23:24:51 -0400

    Deleted old unsused files and url patterns

--- SIRIUS/index/urls.py
+++ SIRIUS/index/urls.py
@@ -9,10 +9,6 @@ urlpatterns = [
 	path("logout/", auth_views.logout, name="logout"),
 	path("register/", views.register, name="register"),
 	path("home/", views.home, name="home"),
-	#path("home/indicator", views.indicator, name="indicator"),
-	#path("home/features", views.features, name="features"),
-	#path("home/subfeatures", views.subfeatures, name="subfeatures"),
-	#path("home/attributes", views.attributes, name="attributes"),
 	
 	# MetaHeuristics
 	path("home/metaheuristics", views.meta_heuristics, name="meta_heuristics"),
